8d58a93a8784701dac688be528370e8368b158ac
Retr0 <sebasmorandez@hotmail.com>
2021-11-02 14:09:38 -0600

    inspire feature

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import discord
+import requests
+import json
 #import os
 #os library works if we hide our token in an online editor
 
 client = discord.Client()
 
+def getQuote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
 
 @client.event
 async def on_ready():
@@ -17,5 +24,9 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('$inspire'):
+        quote = getQuote()
+        await message.channel.send(quote)
 
 client.run('TOKEN')
